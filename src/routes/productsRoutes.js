3e39ac78386f778f51b2915a8af71b26a2046bf7
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2024-04-18 15:13:52 +0200

    adding commentaries

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -4,12 +4,27 @@ const router=express.Router(); //inicializo enrutador de express
 
 const ProductController= require("../controllers/productController.js") //requiero ProductController para hacer funcionar los controladores
 
-router.get("/products",ProductController.getAllProduct );//Devuelve todos los productos [Funciona tanto en Postman como en localhost]
-router.get("/products/:_id",ProductController.getOneProduct );//Devuelve un producto a través del ID [Funciona tanto en Postman como en localhost]
-router.get('/ssr', ProductController.getAllProductSSR );//Devuelve todos los productos haciendo server side rendering
-router.get("/productssr/:_id",ProductController.getOneProductSSR)//Deberia de devolver solo un producto haciendo server side rendering
-router.post("/products", ProductController.postDasboard);//Crea un nuevo producto [Funciona tanto en Postman como en localhost cuando actualizas /products]
-router.post("/products/:_id",ProductController.deleteProduct)//Debe de eliminar un producto insertando el id NO FUNCIONA
-router.put("/products/:_id", ProductController.updateProduct); //Modifica un producto existente desde el id
+//Devuelve todos los productos [Funciona en Postman y localhost]
+router.get("/products",ProductController.getAllProduct );
+
+//Devuelve un producto a través del ID [Funciona en Postman y localhost]
+router.get("/products/:_id",ProductController.getOneProduct );
+
+//Devuelve todos los productos haciendo server side rendering
+router.get('/ssr', ProductController.getAllProductSSR );
+
+//Deberia de devolver solo un producto haciendo server side rendering
+router.get("/productssr/:_id",ProductController.getOneProductSSR)
+
+//Crea un nuevo producto [Funciona en Postman y cuando actualizas /products]
+router.post("/products", ProductController.postDasboard);
+
+//Elimina un producto desde su pagina, los formularios en html, solo tienen dos métodos: 
+//get y post por eso no se utilza router.delete
+router.post("/products/:_id",ProductController.deleteProduct)
+
+//Modifica un producto existente desde el id
+router.put("/products/:_id", ProductController.updateProduct); 
+
 
 module.exports = {router}; //exporto el enrutador --->[exportar y importar de la misma forma!!]
